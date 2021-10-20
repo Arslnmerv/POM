1a72802b0034a4a958efd651095cee2b0e5ef832
Arslnmrv <arslanmerve1619@gmail.com>
2021-10-20 10:39:51 +0300

    pom

---
+++ src/test/java/tests/day13/C02_PagesFaceBook.java
@@ -0,0 +1,26 @@
+package tests.day13;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.FacebookPage;
+import utilities.TestBase;
+
+public class C02_PagesFaceBook extends TestBase {
+
+    @Test
+    public void test() {
+        //facebook login sayfasina gidin
+       driver.get("https://www.facebook.com");
+       FacebookPage facebookPage = new FacebookPage(driver);
+
+        //kullanici adi : techproed
+        facebookPage.mailKutusu.sendKeys("techproed");
+
+        //sifre : 12345
+        facebookPage.sifreKutusu.sendKeys("12345");
+        facebookPage.loginTusu.click();
+
+        //yazdigimizda giris yapamadigimizi test edin
+        Assert.assertTrue(facebookPage.girilemediYaziElementi.isDisplayed());
+    }
+}
