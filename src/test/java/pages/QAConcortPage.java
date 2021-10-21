9b37709407290b1e9d0dccb25501aa9d5910e90d
Arslnmrv <arslanmerve1619@gmail.com>
2021-10-21 18:52:34 +0300

    pom

--- src/test/java/pages/QAConcortPage.java
+++ src/test/java/pages/QAConcortPage.java
@@ -15,42 +15,55 @@ public class QAConcortPage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(linkText = "Log in")
+    @FindBy (linkText = "Log in")
     public WebElement ilkLoginLinki;
 
-    @FindBy(id = "UserName")
+    @FindBy (id = "UserName")
     public WebElement usernameKutusu;
 
-    @FindBy(id = "Password")
+    @FindBy (id = "Password")
     public WebElement passwordKutusu;
 
-    @FindBy(id = "btnSubmit")
+    @FindBy (id = "btnSubmit")
     public WebElement loginButonu;
 
-    @FindBy(xpath = "//span[text()='ListOfUsers']")
+    @FindBy (xpath = "//span[text()='ListOfUsers']")
     public WebElement basariliGirisYaziElementi;
 
     @FindBy(xpath = "//*[.='Try again please']")
     public WebElement loginFailedYazisi;
 
-    @FindBy(xpath = "//span[text()='Hotel Management']")
+    @FindBy (xpath = "//span[text()='Hotel Management']")
     public WebElement hotelManagementLinki;
 
-    @FindBy(partialLinkText = "Hotel List")
+    @FindBy (partialLinkText = "Hotel List")
     public WebElement hotelListLink;
 
-    @FindBy(xpath = "//span[text()='Add Hotel']")
+    @FindBy (className = "hidden-480")
     public WebElement addHotelLink;
     
-    @FindBy(id = "Code")
+    @FindBy (id = "Code")
     public WebElement addHotelCodeKutusu;
 
-    public void ConcortHotelLogin(){
+    @FindBy (id = "IDGroup")
+    public WebElement dropdown;
+
+    @FindBy (id = "btnSubmit")
+    public WebElement saveButton;
+
+    @FindBy (className = "bootbox-body")
+    public WebElement hotelWasInsertedSuccessfullyText;
+
+    @FindBy (xpath = "//button[@data-bb-handler='ok']")
+    public WebElement hotelWasInsertedSuccessfullyTextOkButton;
+
+    public void ConcortHotelLogin() throws InterruptedException {
         Driver.getDriver().get(ConfigReader.getProperty("CHQAUrl"));
         QAConcortPage qaConcortPage=new QAConcortPage();
         qaConcortPage.ilkLoginLinki.click();
-        qaConcortPage.usernameKutusu.sendKeys(ConfigReader.getProperty("CHQAValidUsernamee"));
+        qaConcortPage.usernameKutusu.sendKeys(ConfigReader.getProperty("CHQAValidUsername"));
         qaConcortPage.passwordKutusu.sendKeys(ConfigReader.getProperty("CHQAValidPassword"));
+        Thread.sleep(1000);
         qaConcortPage.loginButonu.click();
     }
 
--- src/test/java/tests/day15/C02_E2ETest.java
+++ src/test/java/tests/day15/C02_E2ETest.java
@@ -1,27 +1,58 @@
 package tests.day15;
 
+import com.github.javafaker.Faker;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.QAConcortPage;
-import utilities.ConfigReader;
 import utilities.Driver;
 
 public class C02_E2ETest {
-    //1. Tests packagenin altına class olusturun: D17_CreateHotel
-    //2. Bir metod olusturun: createHotel
-    //3. https://qa-environment.concorthotel.com/ adresine git.
-    //4. Username textbox ve password metin kutularını locate edin ve asagidaki verileri    girin.
-    //    a. Username : manager
-    //    b. Password  : Manager1!
-    //5. Login butonuna tıklayın.
-    //6. Hotel Management/Hotel List menusunden ADD HOTEL butonuna tiklayin
-    //7. Açılan sayfadaki tüm metin kutularına istediğiniz verileri girin.
-    //8. Save butonuna tıklayın.
-    //9. “Hotel was inserted successfully” textinin göründüğünü test edin.
-    //10. OK butonuna tıklayın.
 
     @Test
-    public void test () {
+    public void test() throws InterruptedException {
+        //1. Tests packagenin altına class olusturun: D17_CreateHotel
+        //2. Bir metod olusturun: createHotel
+        //3. https://qa-environment.concorthotel.com/ adresine git.
+        //4. Username textbox ve password metin kutularını locate edin ve asagidaki verileri    girin.
+        //    a. Username : manager
+        //    b. Password  : Manager1!
+        //5. Login butonuna tıklayın.
         QAConcortPage qaConcortPage = new QAConcortPage();
-       qaConcortPage.ConcortHotelLogin();
+        qaConcortPage.ConcortHotelLogin();
+
+        //6. Hotel Management/Hotel List menusunden ADD HOTEL butonuna tiklayin
+        qaConcortPage.hotelManagementLinki.click();
+        qaConcortPage.hotelListLink.click();
+        qaConcortPage.addHotelLink.click();
+
+        //7. Açılan sayfadaki tüm metin kutularına istediğiniz verileri girin.
+        Actions actions = new Actions(Driver.getDriver());
+        Faker faker = new Faker();
+
+        actions.click(qaConcortPage.addHotelCodeKutusu)
+                .sendKeys(faker.address().zipCode() + Keys.TAB)
+                .sendKeys(faker.name().name() + Keys.TAB)
+                .sendKeys(faker.address().cityName() + Keys.TAB)
+                .sendKeys(faker.phoneNumber().phoneNumber() + Keys.TAB)
+                .sendKeys(faker.internet().emailAddress()).perform();
+
+        actions.click(qaConcortPage.dropdown).perform();
+        Select select = new Select(qaConcortPage.dropdown);
+        select.selectByValue("1");
+
+        //8. Save butonuna tıklayın.
+        qaConcortPage.saveButton.click();
+
+        //9. “Hotel was inserted successfully” textinin göründüğünü test edin.
+        Thread.sleep(1000);
+        Assert.assertTrue(qaConcortPage.hotelWasInsertedSuccessfullyText.isDisplayed(), "“Hotel was inserted successfully” text isn't displayed.");
+
+        //10. OK butonuna tıklayın.
+        qaConcortPage.hotelWasInsertedSuccessfullyTextOkButton.click();
+
+        Driver.closeDriver();
     }
 }
